dec1574a6f67ddefe8d18403efb94a376e14070a
sara rahmani <sararahmani96@gmail.com>
2023-01-30 02:42:24 -0800

    added port to the env

--- database.js
+++ database.js
@@ -9,7 +9,7 @@ const pool = mysql
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
-    port: process.env.MYSQLPORT || 3306,
+    port: process.env.MYSQLPORT || 7120,
   })
   .promise()
 
