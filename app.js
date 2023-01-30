f02f83e876aaa35b3f05a500221c9768cfe73716
sara rahmani <sararahmani96@gmail.com>
2023-01-30 02:06:49 -0800

    added port to the env

--- app.js
+++ app.js
@@ -111,5 +111,5 @@ app.get('*', (req, res) => {
   res.sendFile('dist/index.html', { root: '.' });
 
 });
-const port = 8081
+const port = 8080
 app.listen(port, () => console.log(`listening on port ${port}`))
